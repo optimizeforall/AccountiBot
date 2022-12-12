68bc71776338a25253ce8b240522ec166e4315d6
OptimizeAll <optimizeforall@gmail.com>
2022-12-12 05:23:59 -0600

    Convert all commands to slash commands

--- discord_bot.py
+++ discord_bot.py
@@ -1,13 +1,12 @@
-
-
-# This is new in the discord.py 2.0 update
-
-# imports
+import datetime
+import pickle
+import re
 import discord
 import discord.ext
 import os
-from utils import save_goal, load_goal, valid_format
 from goal import Goal
+from logger import log
+from utils import *
 
 
 def run_bot():
@@ -19,23 +18,18 @@ def run_bot():
         async def on_ready(self):
             print(f'{self.user} has connected to Discord!')
             if not self.synced:
-                await tree.sync(guild = discord.Object(id = 1014983443756613672))
+                await tree.sync(guild=discord.Object(id=1014983443756613672))
                 # self.synced = True
-
 
     client = aclient()
     tree = discord.app_commands.CommandTree(client)
-        
-    @tree.command(name="ping", description="Ping the ping", guild=discord.Object(id=1014983443756613672))
-    async def slashing_commanding(int: discord.Interaction):    
+
+    @tree.command(name="ping", description="Ping the ping")
+    async def slashing_commanding(int: discord.Interaction):
         await int.response.send_message("pongy")
 
-    @tree.command(name="poopy", description="ping p-ping ping ping", guild=discord.Object(id=1014983443756613672))
-    async def slashing_commanding(int: discord.Interaction):    
-        await int.response.send_message("scoopy")
-
     # Create goal
-    @tree.command(name="newgoal", description="Create a new goal!", guild=discord.Object(id=1014983443756613672))
+    @tree.command(name="newgoal", description="Create a new goal! Warning: will override old goal.", guild=discord.Object(id=1014983443756613672))
     async def create_goal(int: discord.Interaction, goal_title: str, goal_days: int, goal_hours: int, days_off: int = 0):
         # Create variables from parsed message
         author_ID = int.user.id
@@ -44,6 +38,10 @@ def run_bot():
         # If days off is greater than goal duration, input invalid, return
         if days_off >= goal_days:
             await int.response.send_message("Days off must be less than goal duration.")
+            return
+        # check if hours is greater than 24*goal_days
+        elif goal_hours > 24*goal_days:
+            await int.response.send_message("Hours must be less than 24* days ;)")
             return
 
         # Crete goal object and save to file with author ID as file name
@@ -54,169 +52,160 @@ def run_bot():
         # Send user initializtion message
         await int.response.send_message(goal.get_init_message())
 
-    # # Add hours 
-    # @client.command(name='ga', help='!ga [time in hours / minutes]')
-    # async def add_time(ctx):
-    #     command_args = ctx.message.content.split()
-    #     author_ID = ctx.message.author.id
-    #     author_name = ctx.message.author.name
+    # Add hours
+    @tree.command(name='addtime', description='Add hours to goal: 1h30, 90m, 1.5h', guild=discord.Object(id=1014983443756613672))
+    async def add_time(int: discord.Interaction, time: str):
 
-    #     if not valid_format(command_args):
-    #         await ctx.send("Invalid format. Use !ga [hours or minutes (h or m)]")
-    #         return
+        author_ID = int.user.id
 
-    #     # Convert string 1h30m to hours
-    #     if 'm' in command_args[1] and not 'h' in command_args[1]:
-    #         hours = float(command_args[1].split('m')[0])/60
-    #     elif 'h' in command_args[1] and not 'm' in command_args[1]:
-    #         hours = float(command_args[1].split('h')[0])
-    #     elif 'h' in command_args[1] and 'm' in command_args[1]:
-    #         hours = float(command_args[1].split(
-    #             'h')[0]) + float(command_args[1].split('h')[1].split('m')[0])/60
-    #     else:
-    #         await ctx.send("Invalid time format. Example: 1h30m, 90m, 1h, 1.5h")
-    #         return
+        # Check if time is formatted correctly: 1h30m, 90m, 1h, 1.5h
+        if not bool(re.match(r"^(\d+(\.\d+)?h)?(\d+(\.\d+)?m)?$", time)):
+            await int.response.send_message("Invalid time format. Example: 1h30m, 90m, 1h, 1.5h")
+            return
 
-    #     # Load goal from file and add hours
-    #     try:
-    #         goal = load_goal(str(author_ID))
-    #         goal.add_hours(datetime.datetime.utcnow(), hours)
-    #         save_goal(str(author_ID), goal)
-    #     except Exception as e:
-    #         log.error("Error loading goal: " + str(e))
-    #         await ctx.send("Error loading goal. Make sure you have the correct title. And have added at least one entry.")
-    #         return
+        # Convert string 1h30m to hours
+        if 'm' in time and not 'h' in time:
+            hours = float(time.split('m')[0])/60
+        elif 'h' in time and not 'm' in time:
+            hours = float(time.split('h')[0])
+        elif 'h' in time and 'm' in time:
+            hours = float(time.split(
+                'h')[0]) + float(time.split('h')[1].split('m')[0])/60
+        else:
+            await int.response.send_message("Invalid time format. Example: 1h30m, 90m, 1h, 1.5h")
+            return
 
-    #     await ctx.send(str(hours) + " hours added to goal: " + goal.goal_title)
-    #     await ctx.send(goal.get_status_message())
+        # Load goal from file and add hours
+        try:
+            goal = load_goal(str(author_ID))
+            goal.add_hours(datetime.datetime.utcnow(), hours)
+            save_goal(str(author_ID), goal)
+        except Exception as e:
+            log.error("Error loading goal: " + str(e))
+            await int.response.send_message("Error loading goal. Make sure you have the correct title. And have added at least one entry.")
+            return
 
-    # # Display progress chart
-    # @client.command(name='gg', help='!gg')
-    # async def display_goal(ctx):
-    #     author_ID = ctx.message.author.id
+        message = str(hours) + " hours added to goal: " + \
+            goal.goal_title + "\n"
+        message += goal.get_status_message()
+        await int.response.send_message(message)
 
-    #     try:
-    #         user_goal = load_goal(str(author_ID))
-    #         user_goal.generate_plot_image()
-    #         await ctx.send(file=discord.File('data/' + str(author_ID) + '.png'))
-    #     except Exception as e:
-    #         log.error("Error loading goal: " + str(e))
-    #         await ctx.send("Error loading goal. Make sure you have the correct title. And have added at least one entry.")
+    # Display progress chart
+    @tree.command(name='progress', description='Display progress chart', guild=discord.Object(id=1014983443756613672))
+    async def display_goal(int: discord.Interaction):
+        author_ID = int.user.id
 
-    # # Delete goal
-    # @client.command(name='gd', help='!gd')
-    # async def delete_goal(ctx):
-    #     command_args = ctx.message.content.split()
-    #     author_ID = ctx.message.author.id
+        try:
+            user_goal = load_goal(str(author_ID))
+            user_goal.generate_plot_image()
+            await int.response.send_message(file=discord.File('data/' + str(author_ID) + '.png'))
+        except Exception as e:
+            log.error("Error loading goal: " + str(e))
+            await int.response.send_message("Error loading goal. Make sure you have the correct title. And have added at least one entry.")
 
-    #     if not valid_format(command_args):
-    #         await ctx.send("Invalid. To delete goal, use !gd [goal title].")
-    #         return
-    #     try:
-    #         os.remove(str(author_ID) + '.pickle')
-    #         await ctx.send("Goal deleted.")
-    #         return
-    #     except Exception as e:
-    #         log.error("Error deleting goal: " + str(e))
-    #         await ctx.send("Error deleting goal. Make sure you have the correct title.")
+    # Delete goal
+    @tree.command(name='deletegoal', description='Delete goal', guild=discord.Object(id=1014983443756613672))
+    async def delete_goal(int: discord.Interaction):
+        author_ID = int.user.id
 
-    # # List all active goals on server
-    # @client.command(name='gl', help='!gl')
-    # async def list_goals(ctx):
-    #     command_args = ctx.message.content.split()
+        try:
+            os.remove('data/goals/' + str(author_ID) + '.pickle')
+            await int.response.send_message("Goal deleted.")
+            return
+        except Exception as e:
+            log.error("Error deleting goal: " + str(e))
+            await int.response.send_message("Error deleting goal. Make sure you have the correct title.")
 
-    #     goals = []
-    #     for filename in os.listdir('./data/goals/'):
-    #         if filename.endswith('.pickle'):
-    #             with open('./data/goals/' + filename, 'rb') as f:
-    #                 goal = pickle.load(f)
-    #                 goals.append(goal)
+    # List all active goals on server
+    @tree.command(name='listgoals', description='List all active goals', guild=discord.Object(id=1014983443756613672))
+    async def list_goals(int: discord.Interaction):
+        goals = []
+        for filename in os.listdir('./data/goals/'):
+            if filename.endswith('.pickle'):
+                with open('./data/goals/' + filename, 'rb') as f:
+                    goal = pickle.load(f)
+                    goals.append(goal)
 
-    #     if len(goals) == 0:
-    #         await ctx.send("No goals found.")
-    #         return
-    #     else:
-    #         message = ""
-    #         for goal in goals:
-    #             hoursRemaining = goal.hourGoal - goal.totalHoursWorked
-    #             message += (
-    #                 f"{goal.author_name}: \"{goal.goal_title}\", "
-    #                 f"{round(hoursRemaining, 2)} hours remaining, with "
-    #                 f"{goal.days_remaining()} days left.\n")
-    #             await ctx.send(message)
+        if len(goals) == 0:
+            await int.response.send_message("No goals found.")
+            return
+        else:
+            message = ""
+            for goal in goals:
+                hours_remaining = goal.hour_goal - goal.total_hours_worked
+                message += (
+                    f"{goal.author_name}: \"{goal.goal_title}\", "
+                    f"{round(hours_remaining, 2)} hours remaining, with "
+                    f"{goal.days_remaining()} days left.\n")
+                await int.response.send_message(message)
 
-    # # Start goal timer
-    # @client.command(name='gstart', help='!gstart')
-    # async def start_goal(ctx):
-    #     try:
-    #         with open('./data/activegoals.pickle', 'rb') as f:
-    #             activeGoals = pickle.load(f)
-    #     except Exception as e:
-    #         log.error("Error loading active goals: " + str(e))
-    #         await ctx.send("Error loading active goals.")
+    # Start goal timer
+    @tree.command(name='start', description='Start goal timer', guild=discord.Object(id=1014983443756613672))
+    async def start_goal(int: discord.Interaction):
+        try:
+            with open('./data/activegoals.pickle', 'rb') as f:
+                activeGoals = pickle.load(f)
+        except Exception as e:
+            log.error("Error loading active goals: " + str(e))
+            await int.response.send_message("Error loading active goals.")
 
-    #     activeGoals[str(ctx.message.author.id)] = datetime.datetime.utcnow()
+        activeGoals[str(int.user.id)] = datetime.datetime.utcnow()
 
-    #     # save active goal to file using pickle
-    #     with open('./data/activegoals.pickle', 'wb') as f:
-    #         pickle.dump(activeGoals, f)
+        # save active goal to file using pickle
+        with open('./data/activegoals.pickle', 'wb') as f:
+            pickle.dump(activeGoals, f)
 
-    #     await ctx.send("Goal started now. Use !gstop to stop goal.")
+        await int.response.send_message("Goal started now. Use /gstop to stop goal.")
 
-    # # Stop goal timer: !gstop
-    # @client.command(name='gstop', help='!gstop')
-    # async def stop_goal(ctx):
-    #     author_ID = ctx.message.author.id
+    # Stop goal timer:
+    @tree.command(name='stop', description='Stop goal timer', guild=discord.Object(id=1014983443756613672))
+    async def stop_goal(int: discord.Interaction):
+        author_ID = int.user.id
 
-    #     try:
-    #         with open('./data/activegoals.pickle', 'rb') as f:
-    #             active_goals = pickle.load(f)
-    #     except Exception as e:
-    #         log.error("Error loading active goals: " + str(e))
-    #         await ctx.send("Error loading active goals.")
-    #         return
+        try:
+            with open('./data/activegoals.pickle', 'rb') as f:
+                active_goals = pickle.load(f)
+        except Exception as e:
+            log.error("Error loading active goals: " + str(e))
+            await int.response.send_message("Error loading active goals.")
+            return
 
-    #     if str(author_ID) in active_goals:
-    #         hoursWorked = (datetime.datetime.utcnow(
-    #         ) - active_goals[str(author_ID)]).total_seconds() / (60 * 60)
+        if str(author_ID) in active_goals:
+            hoursWorked = (datetime.datetime.utcnow(
+            ) - active_goals[str(author_ID)]).total_seconds() / (60 * 60)
 
-    #         try:
-    #             goal = load_goal(str(author_ID))
-    #             goal.add_hours(datetime.datetime.utcnow(), hoursWorked)
-    #             save_goal(str(author_ID), goal)
-    #         except Exception as e:
-    #             log.error("Error loading goal: " + str(e))
-    #             await ctx.send("Error loading goal.")
-    #             return
+            try:
+                goal = load_goal(str(author_ID))
+                goal.add_hours(datetime.datetime.utcnow(), hoursWorked)
+                save_goal(str(author_ID), goal)
+            except Exception as e:
+                log.error("Error loading goal: " + str(e))
+                await int.response.send_message("Error loading goal.")
+                return
 
-    #         del active_goals[str(author_ID)]
-    #         message = f"Goal stopped. {round(hoursWorked, 2)} hours added to goal: {goal.goal_title}\n"
-    #         message += goal.get_status_message()
-    #         await ctx.send(message)
-    #         return
-    #     else:
-    #         await ctx.send("No active goal.")
+            del active_goals[str(author_ID)]
+            message = f"Goal stopped. {round(hoursWorked, 2)} hours added to goal: {goal.goal_title}\n"
+            message += goal.get_status_message()
+            await int.response.send_message(message)
+            return
+        else:
+            await int.response.send_message("No active goal.")
 
-    # # Display active goals status: !gs
-    # @client.command(name='gs', help='!gs')
-    # async def display_active_goals(ctx):
-    #     command_args = ctx.message.content.split()
+    # Display active goals status
+    @tree.command(name='status', description='Display active goals status', guild=discord.Object(id=1014983443756613672))
+    async def display_active_goals(int: discord.Interaction):
+        try:
+            goal = load_goal(str(int.user.id))
+        except Exception as e:
+            log.error("Error loading goal: " + str(e))
+            await int.response.send_message("Error loading goal. Make sure you have active goal.")
+            return
 
-    #     try:
-    #         goal = load_goal(str(ctx.message.author.id))
-    #     except Exception as e:
-    #         log.error("Error loading goal: " + str(e))
-    #         await ctx.send("Error loading goal. Make sure you have active goal.")
-    #         return
+        await int.response.send_message(goal.get_status_message())
 
-    #     await ctx.send(goal.get_status_message())
-
-    # # Display help: !gh
-    # @client.command(name='gh', help='!gh, !help')
-    # async def display_help(ctx):
-    #     command_args = ctx.message.content.split()
-
-    #     if command_args[0] == "!gh" or command_args[0] == "!help" or command_args[0] == "!h":
-    #         await ctx.send(open('./data/help_message.txt', 'r').read())
+    # Display help
+    @tree.command(name='help', description='Display help', guild=discord.Object(id=1014983443756613672))
+    async def display_help(int: discord.Interaction):
+        await int.response.send_message(open('./data/help_message.txt', 'r').read(), ephemeral=True)
 
     client.run(str(os.environ['AccountiBotToken']))
--- utils.py
+++ utils.py
@@ -16,28 +16,3 @@ def save_goal(goalTitle, goal):
             log.info('Saved goal: ' + goalTitle + '.pickle')
     except Exception as e:
         log.error("Error saving goal: " + str(e))
-
-# Determine if user input is formatted correctly
-def valid_format(pm):
-    # !g [goal title] [goal duration in days] [hours to work] ?[rest days]
-    if pm[0] == "!g":
-        if len(pm) != 5 and len(pm) != 4:
-            return False
-        if not pm[2].isdigit() or not pm[3].isdigit():
-            return False
-        elif len(pm) == 5 and not pm[4].isdigit():
-            return False
-
-    # !ga [time in hours / minutes]
-    if pm[0] == "!ga":
-        if len(pm) != 2:
-            return False
-        # Must follow format 1h30m, 90m, 1h, 1.5h
-        if not 'm' in pm[1] and not 'h' in pm[1]:
-            return False
-        
-    # !gg
-    if pm[0] == "!gg":
-        if len(pm) != 2:
-            return False
-    return True
